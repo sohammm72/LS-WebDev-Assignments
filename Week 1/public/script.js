5f1302e76ad87d448c33c6ccf2e77c74999f50f1
sohammm72 <murudkarsoham@gmail.com>
2025-06-18 20:31:33 +0530

    Add Week 1 Assignment

---
+++ Week 1/public/script.js
@@ -0,0 +1,4 @@
+function sike(){
+    document.getElementById("sike").innerText = "BUT NO! \nThanks to some geniuses and our massive bad luck, we are stuck in H6 again because of some construction issues and all our dreams of living in a single room for two years are gone!";
+    document.getElementById("img_sike").src="images/h6-2.jpg"
+}
